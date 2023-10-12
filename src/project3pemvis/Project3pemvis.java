5a9a723edb6614cd53973baefd1051ee13107301
sya-feera <113223505+sya-feera@users.noreply.github.com>
2023-10-12 20:27:19 +0700

    Add files via upload

---
+++ src/project3pemvis/Project3pemvis.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project3pemvis;
+
+/**
+ *
+ * @author firak
+ */
+public class Project3pemvis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
